dc7e042d4650daead4d1437b655a81903a0e8610
Your Name <you@example.com>
2023-03-16 16:53:14 -0700

    4

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", str);
+	return (1);
+}
