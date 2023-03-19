483a9eb24d56b1a7798176c9023aa2983f913554
Your Name <you@example.com>
2023-03-19 13:59:19 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,13 +10,11 @@
 */
 int main(void)
 {
-	char x = '0';
+	int i;
 
-	while (x != ':')
-	{
-		putchar(x);
-		x++;
-	}
+	for (i = 0; i < 10; i++)
+		putchar((i % 10) + '0');
 	putchar('\n');
+
 	return (0);
 }
