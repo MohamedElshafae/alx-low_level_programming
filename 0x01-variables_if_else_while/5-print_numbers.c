96f3fb29bd66cb7e66160083467803ce62c1c38d
Your Name <you@example.com>
2023-03-19 13:49:36 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	printf("%d%d\n", 0, 123456789);
+	return (0);
+}
