cb1e3c668ebc26613a7a0de457bf78827041ef99
Your Name <you@example.com>
2023-03-19 13:41:01 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char x = 'a';
+
+	while (x != '{')
+	{
+		if (x == 'q' || x == 'e')
+		{
+			x++;
+			continue;
+		}
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
