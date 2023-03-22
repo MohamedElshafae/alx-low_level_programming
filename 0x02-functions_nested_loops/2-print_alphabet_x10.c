da6ea94c1e69038c44ab1fb4036ddd4cb43f7233
Elshafae <Elshfae@example.com>
2023-03-21 18:46:33 -0700

    Elshafae

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lower case
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+		for (j = 'a'; j <= 'z'; c++)
+			_putchar(j);
+	_putchar('\n');
+}
