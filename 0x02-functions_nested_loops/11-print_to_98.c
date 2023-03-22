2c82b927f96a272662cbb9417b7e19acd367fcbb
Elshafae <Elshfae@example.com>
2023-03-21 19:56:47 -0700

    Elshafae

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Takes a number and print to 98.
+ * @n: The first number.
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf(n);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	else
+		for (i = n ; i >= 98; i--)
+		{
+			printf(n);
+			if (i != 98)
+				printf(", ");
+		}
+}
