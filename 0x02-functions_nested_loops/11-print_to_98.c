a6af294d264f0d4e64e5265ad52ce7fd984a2284
Elshafae <Elshfae@example.com>
2023-03-21 19:58:17 -0700

    Elshafae

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf(n);
+			printf(i);
 			if (i != 98)
 				printf(", ");
 		}
@@ -21,7 +21,7 @@ void print_to_98(int n)
 	else
 		for (i = n ; i >= 98; i--)
 		{
-			printf(n);
+			printf(i);
 			if (i != 98)
 				printf(", ");
 		}
