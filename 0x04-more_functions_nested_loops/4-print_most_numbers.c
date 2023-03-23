0552c2e740389e873e03eff22439ac1f2d2a01d5
Elshafae <Elshfae@example.com>
2023-03-23 15:53:20 -0700

    elshafae

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,10 +9,7 @@ void print_most_numbers(void)
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
-	{
-		if ((i == '2') || (i == '4'))
-			continue;
-		_putchar(i);
-	}
+		if (!(i == '2' || i == '4'))
+			_putchar(i);
 	_putchar('\n');
 }
