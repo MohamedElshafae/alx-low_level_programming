89ffc2acf78bea6162bfa6bcf08dbde679ae4544
Elshafae <Elshfae@example.com>
2023-03-23 18:23:31 -0700

    elshafae

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,9 +12,9 @@ int main(void)
 	printf("%d", 1);
 	for (i = 2; i < 101; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 			printf(" Fizz");
-		else if (i % 5 == 0)
+		else if (i % 3 != 0 && i % 5 == 0)
 			printf(" Buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf(" FizzBuzz");
