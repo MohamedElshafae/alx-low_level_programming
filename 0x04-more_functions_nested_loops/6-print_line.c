f691e1550fde149255d6b6db1117915ca49524b1
Elshafae <Elshfae@example.com>
2023-03-23 17:42:23 -0700

    elshafae

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,9 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
-		_putchar('_');
-	else
-	{
-		int i;
+	int i;
 
-		for (i = 0; i <= n; i++)
-			_putchar('_');
-		_putchar('\n');
-	}
+	for (i = 0; i <= n; i++)
+		_putchar('_');
+	_putchar('\n');
 }
