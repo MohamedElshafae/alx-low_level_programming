f35915a74f61f18f29d4b4c483ad6604dfcefd02
Elshafae <Elshfae@example.com>
2023-03-23 17:08:35 -0700

    elshafae

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,13 @@
  */
 void more_numbers(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if(j > 9)
+			if (j > 9)
 				_putchar('1');
 			_putchar(j % 10 + '0');
 		}
