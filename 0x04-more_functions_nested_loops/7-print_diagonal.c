c2d22430f329029a287b1c98d0c29ba3ec8c26df
Elshafae <Elshfae@example.com>
2023-03-23 17:57:22 -0700

    elshafae

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print spaces and /
+ * @n: nuber of diagonals
+ * Return:void
+ */
+void print_diagonal(int n)
+{
+	if(n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				if (i != j)
+					_putchar(' ');
+				else
+					_putchar('/');
+			}
+			_putchar('\n');
+		}
+	}
+}
