002221c974d362bc7ac4eff7859140dc7dc8a6da
Elshafae <Elshfae@example.com>
2023-03-23 17:07:02 -0700

    elshafae

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print from 0 - 14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i,j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if(j > 9)
+				_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,4 +7,9 @@ int _isupper(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 #endif /* MAIN_H */
