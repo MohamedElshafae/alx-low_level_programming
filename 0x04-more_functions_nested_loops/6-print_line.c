ca1011ed5da3768eab33e673c49a09763ecb6f34
Elshafae <Elshfae@example.com>
2023-03-23 17:36:01 -0700

    elshafae

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - print underscores
+ * @n: number of _
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n')
+}
