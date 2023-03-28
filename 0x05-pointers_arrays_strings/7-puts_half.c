3564184ab78856b75a554b2db8b846fd7453f544
Elshafae <Elshfae@example.com>
2023-03-27 18:46:04 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,10 +11,10 @@ void puts_half(char *str)
 	int len = strlen(str);
 	int i, start;
 
-	if (len % 2 == 0)
-		start = len / 2;
-	else
+	if (len % 2 != 0)
 		start = (len - 1) / 2;
+	else
+		start = len / 2;
 	for (i = start; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
