9d97c94cec505cefe9a646f0f2ce701e5310c1f5
Elshafae <Elshfae@example.com>
2023-03-27 19:11:54 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 		for (i = len / 2; i < len; i++)
 			_putchar(str[i]);
 	else
-		for (i = (len - 1) / 2; i < len - 1; i++)
+		for (i = (len + 1) / 2; i < len; i++)
 			_putchar(str[i]);
 	_putchar('\n');
 }
