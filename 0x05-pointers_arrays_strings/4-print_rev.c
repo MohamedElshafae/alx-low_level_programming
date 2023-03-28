e5c7e4de1dfa9299ad44439d83bab059cc107e39
Elshafae <Elshfae@example.com>
2023-03-27 18:10:14 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - reverse string
+ * @s: string variable
+ * return: void
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i = len; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
