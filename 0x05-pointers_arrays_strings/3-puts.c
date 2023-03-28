c919e6811bf497d7ea4f9195c39604a38b92363a
Elshafae <Elshfae@example.com>
2023-03-27 17:17:07 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - print string with new line
+ * @str: string variable
+ * Return:void
+ */
+void _puts(char *str)
+{
+	int puts(const char *s);
+
+	puts((char *) str);
+}
