903d5372a52445defe6b36555e655c70996970f9
Elshafae <Elshfae@example.com>
2023-03-29 13:17:34 -0700

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - The strcat function returns a pointer to s1
+ * @dest: A pointer to a string that will be modified. s2 will be copied..
+ * @src: A pointer to a string that will be appended to the end of s1.
+ * Return:The strcat function returns a pointer to s1
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
