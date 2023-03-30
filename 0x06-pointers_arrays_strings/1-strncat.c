0f246d68378a34c90fdc6749f56d6807ce4056e4
Elshafae <Elshfae@example.com>
2023-03-30 16:22:23 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - append string by n
+ * @dest: s1
+ * @src: s2
+ * @n: number
+ * Return: ptr that appended
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len_dest = strlen(dest);
+	int len_src = strlen(src);
+	int i;
+	if (n > len_src)
+		n = len_src;
+	
+	for (i = 0; i < n; i++)
+		dest[len_dest++] = src[i];
+	return (dest);
+}
