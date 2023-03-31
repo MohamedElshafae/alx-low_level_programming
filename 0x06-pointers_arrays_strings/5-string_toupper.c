49a938d06ef67cef189cd3f126ca911891c40578
Elshafae <Elshfae@example.com>
2023-03-30 18:39:27 -0700

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * string_toupper - Tske string and return it in upper case
+ * @c: The string
+ * Return: Uppercase string
+ */
+char *string_toupper(char *c)
+{
+	int i;
+	for (i = 0; c[i] != '\0'; i++)
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] -= 32;
+	return (c);
+}
