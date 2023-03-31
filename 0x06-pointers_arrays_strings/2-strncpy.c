44da46839ed72a6f921bc1b013c6a6c1d6bfcc3c
Elshafae <Elshfae@example.com>
2023-03-30 17:57:50 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int len_src = strlen(src);
 	int i;
-	
+
 	if (n > len_src)
 		n = len_src + 1;
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 	dest[strlen(dest)] = '\0';
-	return (dest);	
+	return (dest);
 }
