03b6d98380c0d1e7e7ac4eb89e9d16ee2cf8c7d1
Elshafae <Elshfae@example.com>
2023-03-30 18:06:57 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,6 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	dest[strlen(dest) - 1] = '\0';
+	while (i < n)
+		dest[i++] = '\0';
 	return (dest);
 }
