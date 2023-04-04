dfe2085a035e284c2cde1b248ba478683e091ff9
Elshafae <Elshfae@example.com>
2023-04-03 18:04:07 -0700

    3-strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn -  Get the length of the initial substring
+ * of the string pointed to by str1 that is made up of
+ * only those character contained in the string pointed to by str2
+ * @s: String That will be scaned
+ * @accept: string containing the characters to match
+ * Return: Returns the number of characters in the initial
+ * segment of str1 which consist only of characters  from str2.
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, max = 0, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					max++;
+			}
+		}
+		else
+		{
+			return (max);
+		}
+	}
+	return (max);
+}
