7939136f3e85867cc75dbdbcf153948c2a83a297
Elshafae <Elshfae@example.com>
2023-04-05 17:36:52 -0700

    1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - print in reverse way
+ *@s:string variable
+ *Return:void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		return;
+}
