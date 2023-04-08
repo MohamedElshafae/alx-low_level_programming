5c09ed423c199173bf0d492aab0263edcb89ffd2
Elshafae <Elshfae@example.com>
2023-04-07 19:21:22 -0700

    3-4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc:the number of argumeent
+ * @argv:string arguments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argv[1] == NULL || argv[2] == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[2]) + atoi(argv[1]) + (0 * argc));
+	return (0);
+}
