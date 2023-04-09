effacb05bb273dd9199ca5785b71c42edc78aa08
Elshafae <Elshfae@example.com>
2023-04-09 16:16:23 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,56 +2,71 @@
 #include <stdlib.h>
 
 /**
- * main - check the code
- * @argc:the number of argumeent
- * @argv:string arguments
- * Return: Always 0.
+ * calc - helper funtion
+ * @cent: theamount of money
+ * Return: the number of changes
+ */
+int calc(int cent)
+{
+	int counter = 0;
+
+	while (cent > 0)
+	{
+		if ((cent / 25) > 0)
+		{
+			counter += cent / 25;
+			cent %= 25;
+		}
+		else if ((cent / 10) > 0)
+		{
+			counter += cent / 10;
+			cent %= 10;
+		}
+		else if ((cent / 5) > 0)
+		{
+			counter += cent / 5;
+			cent %= 5;
+		}
+		else if ((cent / 2) > 0)
+		{
+			counter += cent / 2;
+			cent %= 2;
+		}
+		else
+		{
+			counter += cent;
+			cent = 0;
+		}
+	}
+	return (counter);
+}
+
+/**
+ * main -  prints the minimum number of coins to make change
+ * for an amount of money
+ * @argc: number of args
+ * @argv: array of args
+ * Return: 1 if there is more than 1 arg 0 otherwise
  */
 int main(int argc, char *argv[])
 {
-	int final = 0;
-	int result;
+	int cent, counter;
 
-	if (argc == 1)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else if (atoi(argv[1]) < 0)
+
+	if (atoi(argv[1]) < 0)
 	{
-		printf("%d\n", 0);
+		printf("0\n");
 		return (0);
 	}
-	
-	result = atoi(argv[1]);
-	while (result != 0)
-	{
-		if(result > 25)
-		{
-			final += result / 25;
-			result %= 25;
-		}
-		else if (result > 10)
-		{
-			final += result / 10;
-			result %= 10;
-		}
-		else if (result > 5)
-		{
-			final += result / 5;
-			result %= 5;
-		}
-		else if (result > 2)
-		{
-			final += result / 2;
-			result %= 2;
-		}
-		else
-		{
-			final += result / 1;
-			result %= 1;
-		}
-	}
-	printf ("%d\n", final);
+
+	cent = atoi(argv[1]);
+	counter = calc(cent);
+
+	printf("%d\n", counter);
 	return (0);
 }
