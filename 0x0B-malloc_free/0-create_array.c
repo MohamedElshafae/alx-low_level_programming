553bad8ea732355191a2762f29ad3b8fe0b2e813
Elshafae <Elshfae@example.com>
2023-04-11 14:41:52 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *ptr = malloc(sizeof(char) * size);
 
 	if (ptr == NULL || size == 0)
-		return (ptr);
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
