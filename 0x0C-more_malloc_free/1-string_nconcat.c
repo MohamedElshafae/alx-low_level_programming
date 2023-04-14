8cc70bcf4dc9ae4e4208894d5e641351ca4a8492
Elshafae <Elshfae@example.com>
2023-04-13 17:13:59 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,4 +10,26 @@
  * @n: number of char taken from @s2
  * Return: pointer to the new allocated memory that contains s1 and n of s2
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j = 0;
+	char *ptr;
+
+	if (n >= strlen(s2))
+		n = strlen(s2);
+	ptr = malloc(strlen(s1) + (int)(n));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < strlen(s1) + n; i++)
+	{
+		if (i < strlen(s1))
+			ptr[i] = s1[i];
+		else
+		{
+			ptr[i] = s2[j];
+			j++;
+		}
+	}
+	return (ptr);
+
+}
