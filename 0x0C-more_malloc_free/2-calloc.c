4c5c46f80ade183dc4c5bbc701e6e40aae8636cf
Elshafae <Elshfae@example.com>
2023-04-13 17:38:35 -0700

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,3 +9,17 @@
  * Return: ...
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ptr[i] = 0;
+	return (ptr);
+}
