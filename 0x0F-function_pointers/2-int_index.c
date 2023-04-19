8af1464c4470e07aa026c56a8c76b2bc5c5c7593
Elshafae <Elshfae@example.com>
2023-04-18 23:22:33 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,4 +3,19 @@
 
 /**
  * int_index - searches for an integer.
- *
+ * @array:array of integer
+ * @size:size of an array
+ * @cmp:pointer to the function to be used to compare values
+ * Return:f no element matches, return -1 , If size <= 0, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if ((*cmp)(array[i]))
+			return (i);
+	return (0);
+}
