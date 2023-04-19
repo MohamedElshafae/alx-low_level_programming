a1027bd56ccd1db755d34665e7fab7a5027f439c
Elshafae <Elshfae@example.com>
2023-04-18 22:50:45 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - print name 
+ * @name: string paramiter
+ * @f:pointer to function
+ * Return:void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
