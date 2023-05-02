bc199493ae90a1e5c7fe5baf602fbafed4614566
Elshafae <Elshfae@example.com>
2023-05-02 13:06:57 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,17 +1,16 @@
 #include "lists.h"
 
 /**
- * free_list - delete the list fron the heap
+ * free_listint - delete the list fron the heap
  * @head: head of the list
  */
-void free_list(list_t *head)
+void free_listint(listint_t *head)
 {
-	list_t *p = head;
+	listint_t *p = head;
 
 	while (p)
 	{
 		head = head->next;
-		free(p->str);
 		free(p);
 		p = head;
 	}
