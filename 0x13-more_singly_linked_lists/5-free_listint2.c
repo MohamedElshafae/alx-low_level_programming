fa6b6fc931f4db45462b227342112e6e113dfe59
Elshafae <Elshfae@example.com>
2023-05-02 13:30:41 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *p = *head;
 
-	if (!head)
+	if (head == NULL)
 		return;
-	while (p)
+	while (*head)
 	{
-		*head = (*head)->next;
-		free(p);
-		p = *head;
+		p = (*head)->next;
+		free(*head);
+		*head = p;
 	}
 	*head = NULL;
 }
