62f018a5eeb44ce2a9249a01ce53ff05a0524c77
Elshafae <Elshfae@example.com>
2023-05-08 08:42:01 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,6 +15,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	ssize_t w, r;
 	
+	if (!filename)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
