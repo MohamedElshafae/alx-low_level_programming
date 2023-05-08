fd3fa2f224e134ebe22c96b4c140c0afe66708f7
Elshafae <Elshfae@example.com>
2023-05-08 08:38:30 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * read_textfile - read all text from file
+ * @filename: fileNmae
+ * @letters:the count of leterrs in file
+ * Return: returns the actual number of letters it could read and print
+       if the file can not be opened or read, return 0
+       if filename is NULL return 0
+       if write fails or does not write the expected amount of bytes, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int fd;
+	ssize_t w, r;
+	
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+	r = read(fd, buffer, letters);
+	if (!r)
+		return (0);
+	close(fd);
+	w = write(STDOUT_FILENO, buffer, letters);
+	free(buffer);
+	return(w);
+}
