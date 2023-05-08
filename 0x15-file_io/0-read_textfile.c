767f879c2c1db8266392c0ddebbdea5a694c715d
Elshafae <Elshfae@example.com>
2023-05-08 08:43:50 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,16 +5,12 @@
  * @filename: fileNmae
  * @letters:the count of leterrs in file
  * Return: returns the actual number of letters it could read and print
-       if the file can not be opened or read, return 0
-       if filename is NULL return 0
-       if write fails or does not write the expected amount of bytes, return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	int fd;
-	ssize_t w, r;
-	
+	ssize_t w, r, fd;
+
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
@@ -31,5 +27,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (w == -1)
 		return (0);
 	free(buffer);
-	return(w);
+	return (w);
 }
