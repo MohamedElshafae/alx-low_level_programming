08973a71935b1b3443df71f6e0354072fa867083
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2023-07-07 04:13:15 +0300

    task3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,46 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table you want to add or update the key/value to
+ * @key: is the key, can not be an empty string
+ * @value: is the value associated with the key, must be duplicated
+ * Return: 1 if it succeeded, 0 otherwis
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (!key)
+		return (0);
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (!ht->array[index])
+	{
+		ht->array[index] = new_node;
+		return (1);
+	}
+	else
+	{
+		hash_node_t *ptr = ht->array[index];
+
+		while(ptr)
+		{
+			if (ptr->key == key)
+			{
+				free(new_node);
+				return(0);
+			}
+			ptr = ptr->next;
+		}
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
+		return (1);
+	}
+}
