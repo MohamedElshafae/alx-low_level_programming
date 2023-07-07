182830776d85fe5369f621e328b7cad703d450c7
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2023-07-07 04:15:52 +0300

    fix_error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,12 +12,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 
-	if (!key)
+	if (!key || !value || !ht)
 		return (0);
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
-	
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	index = key_index((const unsigned char *)key, ht->size);
@@ -30,12 +29,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		hash_node_t *ptr = ht->array[index];
 
-		while(ptr)
+		while (ptr)
 		{
 			if (ptr->key == key)
 			{
 				free(new_node);
-				return(0);
+				return (0);
 			}
 			ptr = ptr->next;
 		}
