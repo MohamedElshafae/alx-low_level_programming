c95d36c38bdca94e16ff4c787b1b809b0a1cb0fe
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2023-07-07 07:37:02 +0300

    task6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - that deletes a hash table.
+ * @ht:hash table
+ * Return:void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *cur, *next;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (!ht->array[i])
+			continue;
+		else
+		{
+			cur = ht->array[i];
+			while (cur)
+			{
+				next = cur->next;
+				free(cur->key);
+				free(cur->value);
+				free(cur);
+				cur = next;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
