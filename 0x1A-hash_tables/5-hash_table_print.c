b9fbf8c3438a72d7b58a76f7ab9e07db3950991d
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2023-07-07 05:32:55 +0300

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table.
+ * @ht:hash table
+ * Return:void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *ptr;
+	unsigned int i;
+	int flag = 0;
+
+	if (!ht)
+	{
+		printf("{}\n");
+		return;
+	}
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		if (!ht->array[i])
+			continue;
+		else
+		{
+			ptr = ht->array[i];
+			while (ptr)
+			{
+				if (flag)
+					printf(", ");
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				ptr = ptr->next;
+				flag = 1;
+			}
+		}
+	}
+	printf("}\n");
+}
